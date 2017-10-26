ccd7b5fc7bdfbe413684c0ac6ef7533a8ce8179b
Richard Parker <richard.parker@bristol.ac.uk>
2017-10-26 16:31:39 +0100

    Changed several argument names, and roxygen, comments

--- R/windsimu.R
+++ R/windsimu.R
@@ -3,23 +3,26 @@
 #' Function to create simulated dataset given user-defined parameters and data
 #' structure for growth curve model with optional random effect for L2 variance
 #' 
-#' @param how_many_L2_per_L3 Sample size at level 2 (there isn't an L3, but future compatible in case there ever is!)
-#' @param how_many_L1_per_L2 Sample size at level 1, per level 2 unit
-#' @param time_var_range Vector with min and max for time variable (e.g. \code{c(12, 23)} to range from 12 to 23).
-#' @param var_names Character vector containing names for intercept and time variable (e.g. \code{c("cons", "age")}).
-#' @param x_matrix_A_cons_time Logical vector indicating whether intercept (cons) and time variables to be included in X design matrix A
-#' (i.e. fixed part of model for mean of y); e.g. \code{c(TRUE, FALSE)} indicates intercept, but not time, to be included.
-#' @param z_matrix_A_cons_time As for \code{x_matrix_A_cons_time}, but Z matrix.
-#' @param x_matrix_B_cons_time As for \code{x_matrix_A_cons_time}, but X matrix for level 1 variance function.
-#' @param z_matrix_B_cons_time As for \code{x_matrix_A_cons_time}, but Z matrix for level 1 variance function.
-#' @param beta Vector containing mean for each beta (i.e. i.e. coefficients of fixed effects for model for mean of y).
-#' @param alpha Vector containing mean for each alpha (i.e. i.e. coefficients of fixed effects for level 1 variance function).
-#' @param sigma2u Level 2 covariance matrix; e.g. \code{matrix(c(93, 12, 2.5, 12, 3.8, 0.8, 2.5, 0.8, 0.4), nrow = 3, ncol = 3, byrow = TRUE)}.
-#' @param sigma2e Vector (one element); only specify if 1-level model; defaults to \code{NULL}.
-#' @param log_sigma2e Logical vector indicating whether log link used for level 1 variance function (\code{TRUE}) or not (\code{FALSE}); defaults to \code{TRUE}.
-#' @param linear_sigma2e_attempts Maximum number of attempts made to generate non-negative sigma2e (only applicable when \code{log_sigma2e = FALSE}); defaults to \code{10}.
-#' @param random_seed Random seed adopted.
-#' 
+#' @param sample_size List of named vectors specifying sample size at level 2 and level 1; order not important but need to be named (as \code{n2} and \code{n1}, respectively).
+#' @param var_names Character vector containing names for intercept and time variable (order not important, but elements must be
+#' named \code{x0} (corresponding to intercept) and \code{x1} (time); e.g. \code{c("x0" = "cons", "x1" = "age")}).
+#' @param x1_range Vector with min and max values for time variable (in that order; e.g. \code{c(12, 23)} to range from 12 to 23).
+#' @param design_matrices List of logical vectors indicating whether each variable to be included in design matrices or not; order of elements in each logical vector not important, but must be
+#' named \code{x0} (intercept) and \code{x1} (time). \code{TRUE} indicates variable to be included, \code{FALSE} otherwise. List elements themselves must be named \code{x_A} (corresponding to X matrix (fixed part) of model for mean of y),
+#' \code{z_A} (Z matrix (random part) of model for mean of y), \code{x_B} (X matrix (fixed part) of model for level 1 variance function),
+#' \code{z_B} (Z matrix (random part) of model for level 1 variance function). E.g. \code{list(x_A = c("x0" = TRUE, "x1" = TRUE), z_A = c("x0" = TRUE, "x1" = TRUE), x_B = c("x0" = TRUE, "x1" = TRUE), z_B = c("x0" = TRUE, "x1" = FALSE))}.
+#' @param beta Vector containing mean for each beta (i.e. coefficients of fixed effects for model for mean of y), in order of x0, x1.
+#' @param alpha Vector containing mean for each alpha (i.e. coefficients of fixed effects for level 1 variance function), in order of x0, x1.
+#' @param sigma2u Level 2 covariance matrix. E.g. the vector in \code{matrix(c(93, 12, 2.5, 12, 3.8, 0.8, 2.5, 0.8, 0.4), nrow = 3, ncol = 3, byrow = TRUE)}
+#' would correspond to the following: sigma2u00 (\code{93}), sigma2u01 (\code{12}), sigma2u02 (\code{2.5}), sigma2u01 (\code{12}), sigma2u11 (\code{3.8}), sigma2u12 (\code{0.8}), sigma2u02 (\code{2.5}),
+#' sigma2u12 (\code{0.8}), sigma2u22 (\code{0.4}).
+#' @param sigma2e Numerical vector (one element); only specify if 1-level model; defaults to \code{NULL}.
+#' @param log_sigma2e Logical vector indicating whether log link used for level 1 variance function (\code{TRUE}) or not (\code{FALSE});
+#' defaults to \code{TRUE}.
+#' @param linear_sigma2e_attempts Numerical vector (one element) corresponding to maximum number of attempts made to generate non-negative sigma2e (only applicable when \code{log_sigma2e = FALSE});
+#' defaults to \code{10}.
+#' @param random_seed Numerical vector indicating value of random seed to be set at outset.
+#' @return List containing simulated dataset (as \code{simu_dataframe}), plus other relevant generated objects and user-defined parameters.
 #' @examples
 #' \dontrun{
 #' library(windsimu)
@@ -28,28 +31,29 @@
 #' ## with this term also allowed to covary with random
 #' ## intercept & random slope in level 2 covariance matrix:
 #' simu_u2j_covary <- windsimu(
-#' how_many_L2_per_L3 = 1000,
-#' how_many_L1_per_L2 = 9,
-#' time_var_range = c(-1, 1),
-#' var_names = c("cons", "age"),
-#' x_matrix_A_cons_time = c(TRUE, TRUE),
-#' z_matrix_A_cons_time = c(TRUE, TRUE),
-#' x_matrix_B_cons_time = c(TRUE, TRUE),
-#' z_matrix_B_cons_time = c(TRUE, FALSE),
-#' beta = c(150, 6.5),
-#' alpha = c(-.95, 0.48),
-#' sigma2u =
-#'   matrix(
-#'     c(93, 12, 2.5,
-#'       12, 3.8, 0.8,
-#'       2.5, 0.8, 0.4),
-#'     nrow = 3,
-#'     ncol = 3,
-#'     byrow = TRUE
+#'   sample_size = list(n2 = 1000, n1 = 9),
+#'   var_names = c("x0" = "cons", "x1" = "age"),
+#'   x1_range = c(-1, 1),
+#'   design_matrices = list(
+#'     x_A = c("x0" = TRUE, "x1" = TRUE),
+#'     z_A = c("x0" = TRUE, "x1" = TRUE),
+#'     x_B = c("x0" = TRUE, "x1" = TRUE),
+#'     z_B = c("x0" = TRUE, "x1" = FALSE)
 #'   ),
-#' log_sigma2e = TRUE,
-#' linear_sigma2e_attempts = 10,
-#' random_seed = 1
+#'   beta = c(150, 6.5),
+#'   alpha = c(-.95, 0.48),
+#'   sigma2u =
+#'     matrix(
+#'       c(93, 12, 2.5,
+#'         12, 3.8, 0.8,
+#'         2.5, 0.8, 0.4),
+#'       nrow = 3,
+#'       ncol = 3,
+#'       byrow = TRUE
+#'     ),
+#'   log_sigma2e = TRUE,
+#'   linear_sigma2e_attempts = 10,
+#'   random_seed = 1
 #' )
 #' 
 #' ## inspect structure of returned object:
@@ -63,70 +67,73 @@
 #' ## Level 2 random effect in level 1 variance function
 #' ## but covariance of this term set to 0 in level 2 covariance matrix:
 #' simu_u2j_no_covary <- windsimu(
-#' how_many_L2_per_L3 = 1000,
-#' how_many_L1_per_L2 = 9,
-#' time_var_range = c(-1, 1),
-#' var_names = c("cons", "age"),
-#' x_matrix_A_cons_time = c(TRUE, TRUE),
-#' z_matrix_A_cons_time = c(TRUE, TRUE),
-#' x_matrix_B_cons_time = c(TRUE, TRUE),
-#' z_matrix_B_cons_time = c(TRUE, FALSE),
-#' beta = c(150, 6.5),
-#' alpha = c(-.95, 0.48),
-#' sigma2u =
-#'   matrix(
-#'     c(93, 12, 0,
-#'       12, 3.8, 0,
-#'       0, 0, 0.4),
-#'     nrow = 3,
-#'     ncol = 3,
-#'     byrow = TRUE
+#'   sample_size = list(n2 = 1000, n1 = 9),
+#'   var_names = c("x0" = "cons", "x1" = "age"),
+#'   x1_range = c(-1, 1),
+#'   design_matrices = list(
+#'     x_A = c("x0" = TRUE, "x1" = TRUE),
+#'     z_A = c("x0" = TRUE, "x1" = TRUE),
+#'     x_B = c("x0" = TRUE, "x1" = TRUE),
+#'     z_B = c("x0" = TRUE, "x1" = FALSE)
 #'   ),
-#' sigma2e = NULL,
-#' log_sigma2e = TRUE,
-#' linear_sigma2e_attempts = 10,
-#' random_seed = 1
-#' )
-#' 
-#' 
-#' ## No additional level 2 random effect:
-#' simu_no_u2j <- windsimu(
-#'   how_many_L2_per_L3 = 1000,
-#'   how_many_L1_per_L2 = 9,
-#'   time_var_range = c(-1, 1),
-#'   var_names = c("cons", "age"),
-#'   x_matrix_A_cons_time = c(TRUE, TRUE),
-#'   z_matrix_A_cons_time = c(TRUE, TRUE),
-#'   x_matrix_B_cons_time = c(TRUE, TRUE),
-#'   z_matrix_B_cons_time = c(FALSE, FALSE),
 #'   beta = c(150, 6.5),
 #'   alpha = c(-.95, 0.48),
 #'   sigma2u =
 #'     matrix(
-#'       c(93, 12,
-#'         12, 3.8),
-#'       nrow = 2,
-#'       ncol = 2,
+#'       c(93, 12, 0,
+#'         12, 3.8, 0,
+#'         0, 0, 0.4),
+#'       nrow = 3,
+#'       ncol = 3,
 #'       byrow = TRUE
 #'     ),
+#'   sigma2e = NULL,
 #'   log_sigma2e = TRUE,
 #'   linear_sigma2e_attempts = 10,
 #'   random_seed = 1
 #' )
 #' 
 #' 
+#' ## No additional level 2 random effect:
+#' simu_no_u2j <- windsimu(
+#'   sample_size = list(n2 = 1000, n1 = 9),
+#'   var_names = c("x0" = "cons", "x1" = "age"),
+#'   x1_range = c(-1, 1),
+#'   design_matrices = list(
+#'     x_A = c("x0" = TRUE, "x1" = TRUE),
+#'     z_A = c("x0" = TRUE, "x1" = TRUE),
+#'     x_B = c("x0" = TRUE, "x1" = TRUE),
+#'     z_B = c("x0" = FALSE, "x1" = FALSE)
+#'   ),
+#'     beta = c(150, 6.5),
+#'     alpha = c(-.95, 0.48),
+#'     sigma2u =
+#'       matrix(
+#'         c(93, 12,
+#'           12, 3.8),
+#'         nrow = 2,
+#'         ncol = 2,
+#'         byrow = TRUE
+#'       ),
+#'     log_sigma2e = TRUE,
+#'     linear_sigma2e_attempts = 10,
+#'     random_seed = 1
+#'   )
+#' 
+#' 
 #' ## No log link for level 1 variance function.
 #' ## Note age uncentred (level 1 variance function more likely
 #' ## to stay positive); also note linear_sigma2e_attempts:
 #' simu_u2j_covary_linearL1varfun_Revised <- windsimu(
-#'   how_many_L2_per_L3 = 1000,
-#'   how_many_L1_per_L2 = 9,
-#'   time_var_range = c(11.25, 13.25),
-#'   var_names = c("cons", "age"),
-#'   x_matrix_A_cons_time = c(TRUE, TRUE),
-#'   z_matrix_A_cons_time = c(TRUE, TRUE),
-#'   x_matrix_B_cons_time = c(TRUE, TRUE),
-#'   z_matrix_B_cons_time = c(TRUE, FALSE),
+#'   sample_size = list(n2 = 1000, n1 = 9),
+#'   var_names = c("x0" = "cons", "x1" = "age"),
+#'   x1_range = c(11.25, 13.25),
+#'   design_matrices = list(
+#'     x_A = c("x0" = TRUE, "x1" = TRUE),
+#'     z_A = c("x0" = TRUE, "x1" = TRUE),
+#'     x_B = c("x0" = TRUE, "x1" = TRUE),
+#'     z_B = c("x0" = TRUE, "x1" = FALSE)
+#'   ),
 #'   beta = c(150, 6.5),
 #'   alpha = c(-.95, 0.48),
 #'   sigma2u =
@@ -146,14 +153,10 @@
 #' }
 #'
 #' @export
-windsimu <- function(how_many_L2_per_L3,
-                     how_many_L1_per_L2,
-                     time_var_range,
-                     var_names = c("cons", "age"),
-                     x_matrix_A_cons_time = c(TRUE, TRUE),
-                     z_matrix_A_cons_time = c(TRUE, TRUE),
-                     x_matrix_B_cons_time = c(TRUE, TRUE),
-                     z_matrix_B_cons_time = c(TRUE, FALSE),
+windsimu <- function(sample_size,
+                     var_names,
+                     x1_range,
+                     design_matrices,
                      beta,
                      alpha,
                      sigma2u,
@@ -161,65 +164,70 @@ windsimu <- function(how_many_L2_per_L3,
                      log_sigma2e = TRUE,
                      linear_sigma2e_attempts = 10,
                      random_seed) {
-  
-  library(MASS)
+
   set.seed(random_seed)
   
-  ## ----create scalars, vectors
-  total_L1 <- how_many_L2_per_L3 * how_many_L1_per_L2
-  L2_ID <- rep(1:how_many_L2_per_L3, each = how_many_L1_per_L2)
-  time_var <- rep(seq(from = time_var_range[1], to = time_var_range[2], length.out = how_many_L1_per_L2), times = how_many_L2_per_L3)
-  how_many_variables_x_matrix_A <- sum(x_matrix_A_cons_time)
-  how_many_variables_z_matrix_A <- sum(z_matrix_A_cons_time)
-  how_many_variables_x_matrix_B <- sum(x_matrix_B_cons_time)
-  how_many_variables_z_matrix_B <- sum(z_matrix_B_cons_time)
+  # ensure order of elements consistent ---------------------------
+  # (not necessary to order sample_size as only refer to elements of it by name)
+  var_names <- var_names[c("x0", "x1")]
+  design_matrices <- lapply(design_matrices, function(x) x[c("x0", "x1")])
+  
+  # create vectors ---------------------------
+  total_L1 <- sample_size$n2 * sample_size$n1
+  L2_ID <- rep(1:sample_size$n2, each = sample_size$n1)
+  time_var <- rep(seq(from = x1_range[1], to = x1_range[2], length.out = sample_size$n1), times = sample_size$n2)
+  how_many_variables_x_matrix_A <- sum(design_matrices$x_A)
+  how_many_variables_z_matrix_A <- sum(design_matrices$z_A)
+  how_many_variables_x_matrix_B <- sum(design_matrices$x_B)
+  how_many_variables_z_matrix_B <- sum(design_matrices$z_B)
   predictors <- matrix(nrow = total_L1, ncol = 2)
   predictors[, 1] <- 1 #constant
   predictors[, 2] <- time_var
   
-  ## ----create design matrices, and multiply out (latter for fixed part only):
-  if (any(x_matrix_A_cons_time)){
-    x_matrix_A <- as.matrix(predictors[, x_matrix_A_cons_time])
-    colnames(x_matrix_A) <- var_names[x_matrix_A_cons_time]
+  # create design matrices, and multiply out (latter for fixed part only) ---------------------------
+  if (any(design_matrices$x_A)){
+    x_matrix_A <- as.matrix(predictors[, design_matrices$x_A])
+    colnames(x_matrix_A) <- var_names[design_matrices$x_A]
     fixpart_A <- x_matrix_A %*% beta
   } else {
     x_matrix_A <- NULL
   }
   
-  if (any(z_matrix_A_cons_time)){
-    z_matrix_A <- as.matrix(predictors[, z_matrix_A_cons_time])
-    colnames(z_matrix_A) <- var_names[z_matrix_A_cons_time]
+  if (any(design_matrices$z_A)){
+    z_matrix_A <- as.matrix(predictors[, design_matrices$z_A])
+    colnames(z_matrix_A) <- var_names[design_matrices$z_A]
   } else {
     z_matrix_A <- NULL
   }
   
-  if (any(x_matrix_B_cons_time)){
-    x_matrix_B <- as.matrix(predictors[, x_matrix_B_cons_time])
-    colnames(x_matrix_B) <- var_names[x_matrix_B_cons_time]
+  if (any(design_matrices$x_B)){
+    x_matrix_B <- as.matrix(predictors[, design_matrices$x_B])
+    colnames(x_matrix_B) <- var_names[design_matrices$x_B]
     fixpart_B <- x_matrix_B %*% alpha
   } else {
     x_matrix_B <- NULL
   }
   
-  if (any(z_matrix_B_cons_time)){
-    z_matrix_B <- as.matrix(predictors[, z_matrix_B_cons_time])
-    colnames(z_matrix_B) <- var_names[z_matrix_B_cons_time]
+  if (any(design_matrices$z_B)){
+    z_matrix_B <- as.matrix(predictors[, design_matrices$z_B])
+    colnames(z_matrix_B) <- var_names[design_matrices$z_B]
   } else {
     z_matrix_B <- NULL
   }
 
-  ## ----Create sigma_e; this is all wrapped up in a function (called later),
-  ## as may need to run whole thing repeatedly to get non-negative
-  ## sigma2_e if log_sigma2e = FALSE
+  # Create sigma_e ---------------------------
+  # This is all wrapped up in a function (called later),
+  # as may need to run whole thing repeatedly to get non-negative
+  # sigma2_e if log_sigma2e = FALSE
 
-  create_sigma2_e <- function(how_many_L2_per_L3,
+  create_sigma2_e <- function(n2,
                                 how_many_variables_z_matrix_A,
                                 how_many_variables_z_matrix_B,
                                 sigma2u,
                                 sigma2e,
                                 z_matrix_B = NULL,
                                 fixpart_B) {
-    u <- mvrnorm(n = how_many_L2_per_L3,
+    u <- MASS::mvrnorm(n = sample_size$n2,
                  mu = rep(0, how_many_variables_z_matrix_A + how_many_variables_z_matrix_B),
                  Sigma = sigma2u
     )
@@ -252,7 +260,7 @@ windsimu <- function(how_many_L2_per_L3,
     if (log_sigma2e == FALSE) {
       for (i in linear_sigma2e_attempts) {
         function_call <- create_sigma2_e(
-          how_many_L2_per_L3 = how_many_L2_per_L3,
+          n2 = sample_size$n2,
           how_many_variables_z_matrix_A = how_many_variables_z_matrix_A,
           how_many_variables_z_matrix_B = how_many_variables_z_matrix_B,
           sigma2u = sigma2u,
@@ -270,7 +278,7 @@ windsimu <- function(how_many_L2_per_L3,
       sigma_e <- sqrt(function_call$sigma2_e)
     } else {
       function_call <- create_sigma2_e(
-        how_many_L2_per_L3 = how_many_L2_per_L3,
+        n2 = sample_size$n2,
         how_many_variables_z_matrix_A = how_many_variables_z_matrix_A,
         how_many_variables_z_matrix_B = how_many_variables_z_matrix_B,
         sigma2u = sigma2u,
@@ -281,11 +289,11 @@ windsimu <- function(how_many_L2_per_L3,
     }
   }
   
-  ## ----Create level 1 residuals
+  # Create level 1 residuals ---------------------------
   
   e <- rnorm(n = total_L1, mean = 0, sd = sigma_e)
 
-  ## ----Create randpart_A; generate y
+  # Create randpart_A; generate y ---------------------------
   
   # if 1-level model:
   if (how_many_variables_z_matrix_A + how_many_variables_z_matrix_B == 0) {
@@ -301,12 +309,12 @@ windsimu <- function(how_many_L2_per_L3,
   
   }
   
-  ## ----Create data frame
+  # Create data frame ---------------------------
   L1_ID <- 1:total_L1
   simu_dataframe <- data.frame(L2_ID, L1_ID, y, predictors)
   colnames(simu_dataframe)[4:5] <- var_names
   
-  ## ----Return list containing relevant outputs
+  # Return list containing relevant outputs ---------------------------
   list(
     simu_dataframe = simu_dataframe,
     x_matrix_A = x_matrix_A,
@@ -320,8 +328,10 @@ windsimu <- function(how_many_L2_per_L3,
     u = function_call$u,
     sigma2_e = function_call$sigma2_e,
     sigma_e = sigma_e,
+    log_sigma2e = log_sigma2e,
     alpha = alpha,
     beta = beta,
+    sample_size = sample_size,
     total_L1 = total_L1
   )
 }
