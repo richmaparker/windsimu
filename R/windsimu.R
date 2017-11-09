2084f6044ab4f4a4c78f3b71101ad78f3e59bafc
Richard Parker <richard.parker@bristol.ac.uk>
2017-11-09 09:56:30 +0000

    First attempt at distal outcome - currently binary

--- R/windsimu.R
+++ R/windsimu.R
@@ -7,15 +7,19 @@
 #' @param var_names List of named character vectors indicating names for intercept (\code{x0}) and time variable (\code{x1}),
 #' e.g. \code{list(x0 = "cons", x1 = "age")}.
 #' @param x1_range Vector with min and max values for time variable (in that order: e.g. \code{c(12, 23)} to range from 12 to 23).
-#' @param design_matrices List of four named lists, with each of the latter relating to a design matrix. These four lists are \code{x_A}
+#' @param design_matrices List of five named lists, with each of the latter relating to a design matrix. These five lists are \code{x_A}
 #' (corresponding to X matrix (fixed part) of model for mean of y), \code{z_A} (Z matrix (random part) of model for mean of y),
-#' \code{x_B} (X matrix (fixed part) of model for level 1 variance function), and \code{z_B} (Z matrix (random part) of model for
-#' level 1 variance function). Each of these four lists to contain two named logical vectors (\code{x0} for intercept, \code{x1} for time),
-#' indicating whether each variable to be included in design matrices or not; \code{TRUE} indicates variable to be included, \code{FALSE} otherwise.
+#' \code{x_B} (X matrix (fixed part) of model for level 1 variance function), \code{z_B} (Z matrix (random part) of model for
+#' level 1 variance function) and \code{x_C} (X matrix (fixed part) for model of distal outcome). Each of the first four lists (i.e. \code{x_A},
+#' \code{z_A}, \code{x_B} and \code{z_B}) to contain two named logical vectors (\code{x0} for intercept, \code{x1} for time), indicating whether each variable to be included
+#' in design matrices or not; \code{TRUE} indicates variable to be included, \code{FALSE} otherwise. The last list (\code{x_C}) to contain up to four named logical vectors indicating
+#' whether constant (\code{x0}), and u2j terms (\code{x1}, \code{x2}, \code{x3}) to be included in model for distal outcome or not.
 #' E.g. \code{list(x_A = list(x0 = TRUE, x1 = TRUE), z_A = list(x0 = TRUE, x1 = TRUE), x_B = list(x0 = TRUE, x1 = TRUE), z_B = list(x0 = TRUE, x1 = FALSE))} for each variable
-#' in each design matrix, bar time variable excluded from \code{z_B}.
-#' @param beta Vector containing mean for each beta (i.e. coefficients of fixed effects for model for mean of y), in order of x0, x1.
-#' @param alpha Vector containing mean for each alpha (i.e. coefficients of fixed effects for level 1 variance function), in order of x0, x1.
+#' in each design matrix, bar time variable excluded from \code{z_B}. Note if \code{gamma = NULL}, \code{x_C} is set to \code{NULL}
+#' (i.e. no need to include \code{x_C} in \code{design_matrices} if do not wish to simulate distal outcome).
+#' @param beta Vector containing coefficients of fixed effects for model for mean of y, in order of x0, x1.
+#' @param alpha Vector containing coefficients of fixed effects for level 1 variance function, in order of x0, x1.
+#' @param gamma Vector containing coefficients of fixed effects for model of distal outcome, in order of x0, x1, x2, x3. Defaults to \code{NULL}.
 #' @param sigma2u If the intention is for each level 2 unit to have level 2 residual variance drawn from same covariance matrix, then this need only consists of a single (covariance) matrix.
 #' If the intention is instead to have different patterns of level 2 residual variance for different subgroups of level 2 units, then this needs to consist of a list with two elements:
 #' \code{matrices} and \code{props}. The former (\code{matrices}) is itself a list of covariance matrices: one for each subgroup, whereas the latter (\code{props}) is a
@@ -207,6 +211,40 @@
 #'   linear_sigma2e_attempts = 100,
 #'   random_seed = 1
 #' )
+#' 
+#' ## As simu_u2j_covary (above), but with distal outcome;
+#' ## note x_C in design_matrices, and gamma:
+#' simu_u2j_covary_distal <- windsimu(
+#'   sample_size = list(n2 = 1000, n1 = 9),
+#'   var_names = list(x0 = "cons", x1 = "age"),
+#'   x1_range = c(-1, 1),
+#'   design_matrices = list(
+#'     x_A = list(x0 = TRUE, x1 = TRUE),
+#'     z_A = list(x0 = TRUE, x1 = TRUE),
+#'     x_B = list(x0 = TRUE, x1 = TRUE),
+#'     z_B = list(x0 = TRUE, x1 = FALSE),
+#'     x_C = list(x0 = TRUE, x1 = TRUE, x2 = TRUE, x3 = TRUE)
+#'   ),
+#'   beta = c(150, 6.5),
+#'   alpha = c(-.95, 0.48),
+#'   gamma = c(1, 2, 3, 4),
+#'   sigma2u =
+#'     matrix(
+#'       c(93, 12, 2.5,
+#'         12, 3.8, 0.8,
+#'         2.5, 0.8, 0.4),
+#'       nrow = 3,
+#'       ncol = 3,
+#'       byrow = TRUE
+#'     ),
+#'   log_sigma2e = TRUE,
+#'   linear_sigma2e_attempts = 10,
+#'   random_seed = 1
+#' )
+#' 
+#' 
+#' 
+#' 
 #' }
 #'
 #' @export
@@ -216,6 +254,8 @@ windsimu <- function(sample_size,
                      design_matrices,
                      beta,
                      alpha,
+                     gamma = NULL,
+                     distal_design_matrix = NULL,
                      sigma2u,
                      log_sigma2e = TRUE,
                      linear_sigma2e_attempts = 10,
@@ -223,15 +263,27 @@ windsimu <- function(sample_size,
 
   set.seed(random_seed)
   
-  # return error if incorrect number of sample sizes
+  # return error if incorrect number of sample sizes ---------------------------  
   if (length(sample_size) != 2){
     stop("Incorrect number of sample sizes specified: see sample_size via ?windsimu", call. = FALSE)
   }
 
-  # ensure x0, x1 are in that order; unlist as appropriate ---------------------------
-  # (NB not necessary to order sample_size as refer to elements of that by name only, likewise x_A, z_A. etc., in design_matrices)
+  # ensure elements of design matrices in expected order ---------------------------  
+
+  # ensure x0, x1 are in that order for submodel A and B
+  # (NB not necessary to order sample_size as refer to elements of that by name only,
+  # likewise x_A, z_A. etc., in design_matrices)
   var_names <- unlist(var_names[c("x0","x1")])
-  design_matrices <- lapply(design_matrices, function(x) x[c("x0", "x1")])
+  design_matrices[c("x_A", "z_A", "x_B", "z_B")] <- lapply(design_matrices[c("x_A", "z_A", "x_B", "z_B")], function(x) x[c("x0", "x1")])
+
+  # for submodel C, ensure x_C is NA if no gamma specified; else ensure x0, x1, x2, x3 are in that order
+  if(is.null(gamma)){
+    design_matrices["x_C"] <- NA
+    } else {
+    design_matrices["x_C"] <- lapply(design_matrices["x_C"], function(x) x[c("x0", "x1", "x2", "x3")])
+    }
+
+  # unlist ---------------------------
   design_matrices <- lapply(design_matrices, function(x) unlist(x))
   
   # create vectors ---------------------------
@@ -243,15 +295,17 @@ windsimu <- function(sample_size,
   how_many_variables_x_matrix_B <- sum(design_matrices$x_B)
   how_many_variables_z_matrix_B <- sum(design_matrices$z_B)
 
-  # return error if no variables in z matrix  
+  # return error if no variables in z matrix ---------------------------
   if (how_many_variables_z_matrix_A + how_many_variables_z_matrix_B == 0) {
     stop("Z matrices empty: expects at least one variable (specify via TRUE)", call. = FALSE)
   }
   
+  # create predictors ---------------------------
   predictors <- matrix(nrow = total_L1, ncol = 2)
   predictors[, 1] <- 1 #constant
   predictors[, 2] <- time_var
 
+  # specify subgroups if applicable ---------------------------
   # >1 level 2 covariance matrix? If so, specify quantiles corresponding to subgroup cut-offs;
   # type = 1 in quantile() ensures integers returned (types 1:3 all do that)
   if (is.list(sigma2u)) {
@@ -291,7 +345,7 @@ windsimu <- function(sample_size,
     z_matrix_B <- NULL
   }
 
-  # Create sigma_e ---------------------------
+  # Create sigma2_e, u, randpart_B, var_subgroup ---------------------------
   # This is all wrapped up in a function (called later),
   # as may need to run whole thing repeatedly to get non-negative
   # sigma2_e if user has specified identity link for level 1
@@ -345,10 +399,11 @@ windsimu <- function(sample_size,
          sigma2_e = sigma2_e,
          var_subgroup = var_subgroup
     )
-  }
+  } # end create_sigma2_e()
 
+  # Generate sigma_e when log link for level 1 variance function ---------------------------
     if (log_sigma2e == TRUE) {
-      function_call <- create_sigma2_e(
+      sigma2_e_function_call <- create_sigma2_e(
         n2 = sample_size$n2,
         how_many_variables_z_matrix_A = how_many_variables_z_matrix_A,
         how_many_variables_z_matrix_B = how_many_variables_z_matrix_B,
@@ -357,10 +412,13 @@ windsimu <- function(sample_size,
         fixpart_B = fixpart_B,
         subgroups = subgroups
       )
-      sigma_e <- sqrt(exp(function_call$sigma2_e))
-    } else {  # if (log_sigma2e == FALSE)
+      sigma_e <- sqrt(exp(sigma2_e_function_call$sigma2_e))
+    
+      } else {
+      
+      # Generate sigma_e when identity link for level 1 variance function ---------------------------
       for (i in linear_sigma2e_attempts) {
-        function_call <- create_sigma2_e(
+        sigma2_e_function_call <- create_sigma2_e(
           n2 = sample_size$n2,
           how_many_variables_z_matrix_A = how_many_variables_z_matrix_A,
           how_many_variables_z_matrix_B = how_many_variables_z_matrix_B,
@@ -369,7 +427,8 @@ windsimu <- function(sample_size,
           fixpart_B = fixpart_B,
           subgroups = subgroups
         )
-        if (all(function_call$sigma2_e >= 0)) {
+
+        if (all(sigma2_e_function_call$sigma2_e >= 0)) {
           break
         }
         if (i == linear_sigma2e_attempts)
@@ -377,7 +436,7 @@ windsimu <- function(sample_size,
             "No viable simulations of sigma2e achieved within max number of iterations specified."
           )
       }
-      sigma_e <- sqrt(function_call$sigma2_e)
+      sigma_e <- sqrt(sigma2_e_function_call$sigma2_e)
     }
 
   # Create level 1 residuals ---------------------------
@@ -386,35 +445,65 @@ windsimu <- function(sample_size,
 
   # Create randpart_A; generate y ---------------------------
   
-  randpart_A <- rowSums(z_matrix_A * function_call$u[L2_ID, c("u0j", "u1j")])
+  randpart_A <- rowSums(z_matrix_A * sigma2_e_function_call$u[L2_ID, c("u0j", "u1j")])
   
   y <- fixpart_A + randpart_A + e
-  
-  # Create data frame ---------------------------
+
+  # Create L1 data frame ---------------------------
   L1_ID <- 1:total_L1
   simu_dataframe <- data.frame(L2_ID, L1_ID, y, predictors)
   colnames(simu_dataframe)[4:5] <- var_names
-  if (!is.null(function_call$var_subgroup)){ # if applicable, add indicator for variance subgroups
-    simu_dataframe <- cbind(simu_dataframe, var_subgroup = function_call$var_subgroup)
+  if (!is.null(sigma2_e_function_call$var_subgroup)){ # if applicable, add indicator for variance subgroups
+    simu_dataframe <- cbind(simu_dataframe, var_subgroup = sigma2_e_function_call$var_subgroup)
   }
+  
+  # Create distal outcome; add to L1 data frame and create L2 data frame ---------------------------
+  if(!is.null(gamma)){
+      L2_cons <- rep(1, times = sample_size$n2)
+      predictors_C <- cbind(L2_cons, sigma2_e_function_call$u) # append u to cons
+      x_matrix_C <- predictors_C[, design_matrices$x_C] # only keep those predictors user requested
+      # multiply matrix with vector (gamma) (row-wise, via sweep()),
+      # then add up (row-wise, via rowSums()):
+      fixpart_C <- rowSums(
+        sweep(
+          x_matrix_C, MARGIN = 2, gamma, `*`
+          )
+        )
+      pr = 1 / (1 + exp(-fixpart_C))
+      distal_y = rbinom(sample_size$n2, 1, pr)
+      simu_dataframe <- cbind(simu_dataframe, distal_y = distal_y[L2_ID])
+      L2_ID_B <- unique(L2_ID)
+      simu_dataframe_distal <- data.frame(cbind(L2_ID = L2_ID_B, distal_y, cons = L2_cons, sigma2_e_function_call$u))
+      
+  } else {
+    # if user does not require distal outcome:
+    x_matrix_C <- NULL
+    fixpart_C <- NULL
+    simu_dataframe_distal <- NULL
+  }
+  
   # Return list containing relevant outputs ---------------------------
   list(
     simu_dataframe = simu_dataframe,
+    simu_dataframe_distal = simu_dataframe_distal,
     sample_size = sample_size,
     total_L1 = total_L1,
     x_matrix_A = x_matrix_A,
     z_matrix_A = z_matrix_A,
     x_matrix_B = x_matrix_B,
     z_matrix_B = z_matrix_B,
+    x_matrix_C = x_matrix_C,
     fixpart_A = fixpart_A,
-    fixpart_B = fixpart_B,
     randpart_A = randpart_A,
-    randpart_B = function_call$randpart_B,
-    u = function_call$u,
-    sigma2_e = function_call$sigma2_e,
+    fixpart_B = fixpart_B,
+    randpart_B = sigma2_e_function_call$randpart_B,
+    fixpart_C = fixpart_C,
+    u = sigma2_e_function_call$u,
+    sigma2_e = sigma2_e_function_call$sigma2_e,
     sigma_e = sigma_e,
     beta = beta,
     alpha = alpha,
+    gamma = gamma,
     sigma2u = sigma2u,
     log_sigma2e = log_sigma2e,
     linear_sigma2e_attempts = linear_sigma2e_attempts
